8b7a418c3ef96b10e900fae2d03dd78f892ae1e8
DuWenLiang <739285514@qq.com>
2017-10-23 17:10:20 +0800

    Initial Commit

---
+++ VirtualImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VirtualImage
+//
+//  Created by 杜文亮 on 2017/10/23.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VirtualImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VirtualImage
+//
+//  Created by 杜文亮 on 2017/10/23.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
